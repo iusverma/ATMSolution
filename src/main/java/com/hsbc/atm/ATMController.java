625b9ffdac6899cddb41d5da5d3d03ae724cd177
Verma, Ayush <ayverma@paypalcorp.com>
2018-06-04 21:51:33 +0800

    compilation issue fix

--- src/main/java/com/hsbc/atm/ATMController.java
+++ src/main/java/com/hsbc/atm/ATMController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hsbc.atm.model.Request;
 import com.hsbc.atm.model.Response;
-import com.hsbc.atm.model.WithdrawRequest;
 import com.hsbc.atm.model.WithdrawResponse;
 
 /**
@@ -23,12 +23,12 @@ public class ATMController {
 	private static final Logger LOGGER = Logger.getLogger(ATMController.class);
 
 	/**
-	 * withdrawRequest
+	 * Withdraw Request
 	 * all withdrawal request comes here,
 	 * responds with the status and balance
 	 */
 	@RequestMapping(value="/amount", method=RequestMethod.POST)
-	public Response withdrawAmount(@RequestBody WithdrawRequest request) {
+	public Response withdrawAmount(@RequestBody Request request) {
 		LOGGER.debug("withdrawAmount - begins");
 		return new WithdrawResponse();
 	}
